0291a047113cb7d983a9c5587309fd6c1abc0007
Cardan16vc@outlook.es <Cardan16vc@outlook.es>
2022-05-11 00:02:12 -0600

    Agregar archivos de proyecto.

---
+++ Electrónicos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RepasoParcial2
+{
+    public class Electrónico : Producto
+    {
+        public int voltage { get; set; }
+        public DateTime WarrantyTime { get; set; } 
+    }
+}
---
+++ Libros.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RepasoParcial2
+{
+    public class Libro : Producto 
+    {
+        public string Editorial { get; set; }
+        public string Category { get; set; }
+        public string Author { get; set; }
+    }
+}
